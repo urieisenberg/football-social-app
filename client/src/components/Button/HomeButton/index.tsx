89a46ea6228680b53d0208afca384122ff0b0693
urieisenberg <urieisenberg@gmail.com>
2022-12-11 16:28:09 +0200

    Home button

---
+++ client/src/components/Button/HomeButton/index.tsx
@@ -0,0 +1,12 @@
+import { useNavigate } from 'react-router-dom';
+import { IconButton, IconButtonProps } from '../Icon';
+import { GoHome } from 'react-icons/go';
+
+export const HomeButton = ({ size, home }: IconButtonProps) => {
+  const navigate = useNavigate();
+  return (
+    <IconButton onClick={() => navigate('/welcome')} home={true}>
+      <GoHome size={size} />
+    </IconButton>
+  );
+};
