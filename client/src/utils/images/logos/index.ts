37a76e99bc9c996fb610d0d7bc1c00e677a67638
urieisenberg <urieisenberg@gmail.com>
2022-12-12 12:06:24 +0200

    team logos

---
+++ client/src/utils/images/logos/index.ts
@@ -0,0 +1,49 @@
+import atalanta from "./atalanta.png";
+import bolonga from "./bologna.png";
+import cagliari from "./cagliari.png";
+import empoli from "./empoli.png";
+import fiorentina from "./fiorentina.png";
+import hellasVerona from "./hellas-verona.png";
+import lazio from "./lazio.png";
+import napoli from "./napoli.png";
+import roma from "./roma.png";
+import sassuolo from "./sassuolo.png";
+import sampdoria from "./sampdoria.png";
+import salernitana from "./salernitana.png";
+import torino from "./torino.png";
+import udinese from "./udinese.png";
+import venezia from "./venezia.png";
+import inter from "./inter.png";
+import milan from "./milan.png";
+import juventus from "./juve.png";
+import spezia from "./spezia.png";
+import genoa from "./genoa.png";
+import lecce from "./lecce.png";
+import cremonese from "./cremonese.png";
+import monza from "./monza.png";
+
+export {
+  atalanta,
+  bolonga,
+  cagliari,
+  empoli,
+  fiorentina,
+  hellasVerona,
+  lazio,
+  napoli,
+  roma,
+  sassuolo,
+  sampdoria,
+  salernitana,
+  torino,
+  udinese,
+  venezia,
+  inter,
+  milan,
+  juventus,
+  spezia,
+  genoa,
+  lecce,
+  cremonese,
+  monza,
+};
