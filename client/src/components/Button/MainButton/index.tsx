bc14ff397b95c2ed556c3d9d7d057629b41b7d47
urieisenberg <urieisenberg@gmail.com>
2022-12-13 21:05:49 +0200

    update onClick

--- client/src/components/Button/MainButton/index.tsx
+++ client/src/components/Button/MainButton/index.tsx
@@ -3,7 +3,7 @@ import { TextButton, TextButtonProps } from '../Text';
 interface MainButtonProps extends TextButtonProps {
   text?: string;
   icon?: React.ReactElement;
-  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 } 
 export const MainButton = ({
   text,
