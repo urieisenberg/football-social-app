5f89c38c0d01f408f05cf93a894c46121c556945
urieisenberg <urieisenberg@gmail.com>
2022-12-14 19:47:24 +0200

    update icons for tickets

--- client/src/components/Form/Input/Icons.tsx
+++ client/src/components/Form/Input/Icons.tsx
@@ -1,18 +1,20 @@
 import { GoMail, GoLock } from 'react-icons/go';
-import { AiOutlineUser } from 'react-icons/ai';
+import { AiOutlineUser, AiOutlineMail } from 'react-icons/ai';
 
 export interface InputIconsProps {
-  type: 'username' | 'email' | 'password';
+  type: 'username' | 'email' | 'password' | 'message';
 }
 
 export const InputIcons = ({ type }: InputIconsProps) => {
   switch (type) {
     case 'username':
-      return <AiOutlineUser size={20}/>;
+      return <AiOutlineUser size={20} />;
     case 'email':
       return <GoMail size={20} />;
     case 'password':
       return <GoLock size={20} />;
+    case 'message':
+      return <AiOutlineMail size={20} />;
     default:
       return null;
   }
