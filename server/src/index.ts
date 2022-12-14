5a5db7b88abb14528cc02bdb66c70fe5bfae7974
urieisenberg <urieisenberg@gmail.com>
2022-12-14 17:45:48 +0200

    ticketRoutes

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import 'colors';
 import { connectDB } from './config/connectDB';
-import { authRoutes } from './routes';
+import { authRoutes, ticketRoutes } from './routes';
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
@@ -24,6 +24,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/tickets', ticketRoutes);
 
 app.listen(port, () => {
   console.log(`~~ Server running on port ${port} ~~`.green.bold);
