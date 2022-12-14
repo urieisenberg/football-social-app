91a2b6d088afc3fe7898a981e3c4f5acbd64d36e
urieisenberg <urieisenberg@gmail.com>
2022-12-14 16:15:22 +0200

    teams db

---
+++ client/src/utils/db/teams.ts
@@ -0,0 +1,116 @@
+import * as logos from "../images/logos";
+
+export const teams = [
+  {
+    name: "AC Milan",
+    id: 489,
+    logo: logos.milan,
+  },
+  {
+    name: "Inter Milan",
+    id: 505,
+    logo: logos.inter,
+  },
+  {
+    name: "Napoli",
+    id: 492,
+    logo: logos.napoli,
+  },
+  {
+    name: "Juventus",
+    id: 496,
+    logo: logos.juventus,
+  },
+  {
+    name: "Lazio",
+    id: 487,
+    logo: logos.lazio,
+  },
+  {
+    name: "Roma",
+    id: 497,
+    logo: logos.roma,
+  },
+  {
+    name: "Fiorentina",
+    id: 502,
+    logo: logos.fiorentina,
+  },
+  {
+    name: "Atalanta",
+    id: 499,
+    logo: logos.atalanta,
+  },
+  {
+    name: "Hellas Verona",
+    id: 504,
+    logo: logos.hellasVerona,
+  },
+  {
+    name: "Torino",
+    id: 503,
+    logo: logos.torino,
+  },
+  {
+    name: "Sassuolo",
+    id: 488,
+    logo: logos.sassuolo,
+  },
+  {
+    name: "Udinese",
+    id: 494,
+    logo: logos.udinese,
+  },
+  {
+    name: "Bologna",
+    id: 500,
+    logo: logos.bolonga,
+  },
+  {
+    name: "Empoli",
+    id: 511,
+    logo: logos.empoli,
+  },
+  {
+    name: "Sampdoria",
+    id: 498,
+    logo: logos.sampdoria,
+  },
+  {
+    name: "Spezia",
+    id: 515,
+    logo: logos.spezia,
+  },
+  {
+    name: "Salernitana",
+    id: 514,
+    logo: logos.salernitana,
+  },
+  {
+    name: "Cagliari",
+    id: 490,
+    logo: logos.cagliari,
+  },
+  {
+    name: "Genoa",
+    id: 495,
+    logo: logos.genoa,
+  },
+  {
+    name: "Cremonese",
+    id: 520,
+    logo: logos.cremonese,
+  },
+  {
+    name: "Lecce",
+    id: 867,
+    logo: logos.lecce,
+  },
+  {
+    name: "Monza",
+    id: 1579,
+    logo: logos.monza,
+  },
+];
+
+
