93be006903700139d06384a487fea63e1dca92cd
urieisenberg <urieisenberg@gmail.com>
2022-12-14 17:48:16 +0200

    note model

---
+++ server/src/models/noteModel.ts
@@ -0,0 +1,40 @@
+import { Schema, model, Document } from 'mongoose';
+
+interface INote extends Document {
+  user: {
+    type: Schema.Types.ObjectId;
+    ref: 'User';
+    required: true;
+  };
+  ticket: {
+    type: Schema.Types.ObjectId;
+    ref: 'Ticket';
+    required: true;
+  };
+  text: {
+    type: string;
+    required: true;
+  };
+}
+
+const noteSchema: Schema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    ticket: {
+      type: Schema.Types.ObjectId,
+      ref: 'Ticket',
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+      min: 5,
+      max: 50,
+    },
+  },
+  { timestamps: true }
+);
