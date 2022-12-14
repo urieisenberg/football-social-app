31d0469c8368e2c974d04b5e36b075ce83bb2fbc
urieisenberg <urieisenberg@gmail.com>
2022-12-14 16:15:41 +0200

    init routes

--- client/src/routes/Routes.tsx
+++ client/src/routes/Routes.tsx
@@ -2,15 +2,32 @@ import {
   Routes as Routing,
   Route,
   Navigate,
+  useLocation,
 } from 'react-router-dom';
+import { Protected } from './Protected';
+import { Public } from './Public';
 import { Welcome } from '../pages/Welcome';
+import { Register } from '../features/auth/Register';
+import { Login } from '../features/auth/Login';
+import { NotFound } from '../pages/NotFound';
 
 export const Routes = () => {
+  const location = useLocation();
   return (
-    <Routing>
-      <Route path="/" element={<Welcome />} />
-      {/* <Route path="welcome" element={<Welcome />} />
-      <Route path="/welcome" element={<Navigate to="/" replace />} /> */}
+    <Routing location={location} key={location.pathname}>
+      {/* Protected Routes */}
+      <Route path="/" element={<Protected />}>
+        <Route path="/" element={<Navigate to="league/135" />} />
+        {/* <Route path="league/:leagueid/*" element={<League />} /> */}
+        <Route path="*" element={<NotFound />} />
+      </Route>
+      {/* Public Routes */}
+      <Route path="welcome" element={<Public />}>
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/welcome" />} />
+      </Route>
     </Routing>
   );
 };
