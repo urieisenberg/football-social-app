47aba5650fc8cacc7e171d50f5def18612395f9e
urieisenberg <urieisenberg@gmail.com>
2022-12-14 19:49:40 +0200

    ticket reducer added

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query/react';
 import { authApi } from './services/auth';
 import authReducer from '../features/auth/authSlice';
+import ticketReducer from '../features/tickets/ticketsSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     [authApi.reducerPath]: authApi.reducer,
+    ticket: ticketReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(authApi.middleware),
