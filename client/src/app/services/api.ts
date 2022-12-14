785922a771fbde189b610817ea005b9a527799dc
urieisenberg <urieisenberg@gmail.com>
2022-12-14 16:11:05 +0200

    main api for server using rtq

---
+++ client/src/app/services/api.ts
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { RootState } from '../store';
+
+export const api = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:3000/api',
+    prepareHeaders: (headers, { getState }) => {
+      const user = (getState() as RootState).auth?.user;
+      if (user) {
+        headers.set('authorization', `Bearer ${user}`);
+      }
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({}),
+});
