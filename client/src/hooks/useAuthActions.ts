886f34101ab99aa921509d733925ae2d7826854b
urieisenberg <urieisenberg@gmail.com>
2022-12-15 23:22:33 +0200

    useAuthActions to deal with setuser and logout

---
+++ client/src/hooks/useAuthActions.ts
@@ -0,0 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch } from './useAppDispatch';
+import { setUser as login, logout as setLogout } from '../features/auth/authSlice';
+import { User } from '../app/types';
+
+export const useAuthActions = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const setUser = (user: User) => {
+    dispatch(login(user));
+    navigate('/');
+  };
+
+  const logout = () => {
+    dispatch(setLogout());
+    navigate('/welcome');
+  };
+
+  return { setUser, logout };
+};
