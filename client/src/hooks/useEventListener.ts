15ef96ac3b29f8ebe9b5820439b7d86dbd6eb7d1
urieisenberg <urieisenberg@gmail.com>
2022-12-15 23:21:31 +0200

    useEventListener hook for sidebar use?

---
+++ client/src/hooks/useEventListener.ts
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from "react";
+
+export const useEventListener = (
+  eventType: string,
+  handler: any,
+  element = window
+) => {
+  const handlerRef = useRef(handler);
+
+  useEffect(() => {
+    handlerRef.current = handler;
+  }, [handler]);
+
+  useEffect(() => {
+    if (element == null) return;
+    const eventListener = (event: any) => handlerRef.current(event);
+    element.addEventListener(eventType, eventListener);
+
+    return () => {
+      element.removeEventListener(eventType, eventListener);
+    };
+  }, [eventType, element]);
+};
