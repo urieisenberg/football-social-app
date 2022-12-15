fdd5948aab435e5208ba1d61746bcd1636c3ed37
urieisenberg <urieisenberg@gmail.com>
2022-12-15 23:22:06 +0200

    fix and clean useAuth hook

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -1,40 +1,8 @@
-// import { useMemo } from 'react';
-// import { useToggle } from './useToggle';
-// import { useTypedSelector } from './useTypedSelector';
-// import { selectCurrentUser } from '../features/auth/authSlice';
-
-// export const useAuth = () => {
-//   const [checkingAuthStatus, toggleCheckingAuthStatus] = useToggle(true);
-
-//   const user = localStorage.getItem('user') ? true : false;
-
-//   return useMemo(() => ({ user }), [user]);
-// };
-
 import { useMemo } from 'react';
-import { useAppDispatch } from './useAppDispatch';
 import { useAppSelector } from './useAppSelector';
-import { setUser } from '../features/auth/authSlice';
 
 export const useAuth = () => {
-  const dispatch = useAppDispatch();
   const user = useAppSelector((state) => state.auth.user);
-
-  const storedUser = useMemo(() => {
-    const user = localStorage.getItem('user');
-    if (user && user !== 'null') {
-      return JSON.parse(user);
-    }
-    return null;
-  }, []);
-
-  const isAuthenticated = useMemo(() => storedUser !== null, [storedUser]);
-
-  useMemo(() => {
-    if (storedUser) {
-      dispatch(setUser(storedUser));
-    }
-  }, [dispatch, storedUser]);
-
-  return { user, isAuthenticated, storedUser };
+  const isAuthenticated = useMemo(() => user !== null, [user]);
+  return { user, isAuthenticated };
 };
