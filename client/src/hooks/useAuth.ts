26edd4e9040afddc0645d4bd30adda14af06b938
urieisenberg <urieisenberg@gmail.com>
2022-12-15 15:05:05 +0200

    storedUser to parse user only if user changs

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -1,12 +1,30 @@
+// import { useMemo } from 'react';
+// import { useToggle } from './useToggle';
+// import { useTypedSelector } from './useTypedSelector';
+// import { selectCurrentUser } from '../features/auth/authSlice';
+
+// export const useAuth = () => {
+//   const [checkingAuthStatus, toggleCheckingAuthStatus] = useToggle(true);
+
+//   const user = localStorage.getItem('user') ? true : false;
+
+//   return useMemo(() => ({ user }), [user]);
+// };
+
 import { useMemo } from 'react';
-import { useToggle } from './useToggle';
-import { useTypedSelector } from './useTypedSelector';
-import { selectCurrentUser } from '../features/auth/authSlice';
+import { useAppDispatch } from './useAppDispatch';
+import { useAppSelector } from './useAppSelector';
+import { setUser } from '../features/auth/authSlice';
 
 export const useAuth = () => {
-  const [checkingAuthStatus, toggleCheckingAuthStatus] = useToggle(true);
-
-  const user = localStorage.getItem('user') ? true : false;
-
-  return useMemo(() => ({ user }), [user]);
+  const dispatch = useAppDispatch();
+  const user = useAppSelector((state) => state.auth.user);
+ 
+  const storedUser = useMemo(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser && storedUser !== 'null') {
+      return JSON.parse(storedUser);
+    }
+    return null;
+  }, []);
 };
