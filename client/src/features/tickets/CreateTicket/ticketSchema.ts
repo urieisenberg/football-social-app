7d86f160229b01dab80e09c06555ec466c0afbdc
urieisenberg <urieisenberg@gmail.com>
2022-12-16 22:20:30 +0200

    subject is required

--- client/src/features/tickets/CreateTicket/ticketSchema.ts
+++ client/src/features/tickets/CreateTicket/ticketSchema.ts
@@ -1,12 +1,11 @@
 import * as Yup from 'yup';
 
 export const ticketSchema = Yup.object().shape({
-  subject: Yup.string().oneOf(
-    ['general', 'bug', 'feature', 'other'],
-    'Subject is required'
-  ),
+  subject: Yup.string()
+    .oneOf(['general', 'bug', 'feature', 'other'])
+    .required('Subject is required'),
   message: Yup.string()
-    .required('Message is required')
-    .min(5, 'Message must be at least 5 characters')
-    .max(50, 'Message must be less than 50 characters'),
+    .required('Description is required')
+    .min(5, 'Description must be at least 5 characters')
+    .max(50, 'Description must be less than 50 characters'),
 });
