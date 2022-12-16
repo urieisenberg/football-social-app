7abdb9fa60c682abcabe12e9ab7ad5a2c2cad53b
urieisenberg <urieisenberg@gmail.com>
2022-12-16 22:20:47 +0200

    update token to auth state

--- client/src/app/types/User.ts
+++ client/src/app/types/User.ts
@@ -1,34 +1,34 @@
 export interface User {
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+  team: {
     id: number;
-    username: string;
-    email: string;
-    password: string;
-    team: {
-      id: number;
-      name: string;
-      logo: string;
-    };
-    profilePicture: string;
-    follows: Array<string>;
-    followers: Array<string>;
-    favFixtures: Array<string>;
-  }
-  
-  export interface AuthState {
-    user: User | null;
-  }
-  
-  export interface RegisterUser extends LoginUser{
-    username: string;
-    team: {
-      id: number;
-      name: string;
-      logo: string;
-    };
-  }
-  
-  export interface LoginUser {
-    email: string;
-    password: string;
-  }
-  
+    name: string;
+    logo: string;
+  };
+  profilePicture: string;
+  follows: Array<string>;
+  followers: Array<string>;
+  favFixtures: Array<string>;
+}
+
+export interface AuthState {
+  user: User | null;
+  token: string | null;
+}
+
+export interface RegisterUser extends LoginUser {
+  username: string;
+  team: {
+    id: number;
+    name: string;
+    logo: string;
+  };
+}
+
+export interface LoginUser {
+  email: string;
+  password: string;
+}
