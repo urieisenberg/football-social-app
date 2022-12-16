1b79d76d2d38795bd3e9d1f56c5b39480e6b4be1
urieisenberg <urieisenberg@gmail.com>
2022-12-16 22:04:37 +0200

    styles for reusable nav

---
+++ client/src/components/Nav/styles.ts
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import { Nav, Navbar, Container } from 'react-bootstrap';
+
+interface NavItemProps {
+  hide?: string;
+}
+
+interface NavLinkProps {
+  active?: boolean;
+}
+
+export const NavWrapper = styled(Navbar).attrs({
+  expand: 'lg',
+  className: 'container text-center',
+})`
+  background-color: ${({ theme }) => theme.background};
+  color: ${({ theme }) => theme.text};
+`;
+
+export const NavContainer = styled(Container)`
+  margin: 0 auto;
+  @media (max-width: 990px) {
+    display: inline-block;
+  }
+`;
+
+export const NavLink = styled(Nav.Link)<NavLinkProps>`
+  color: ${({ active, theme }) => (active ? theme.info : theme.text)};
+  font-weight: ${({ active }) => (active ? 'bold' : 'normal')};
+  text-decoration: none;
+  text-transform: uppercase;
+  &:hover {
+    color: ${({ theme }) => theme.info};
+  }
+`;
+
+export const NavItem = styled(motion.span).attrs({
+  whileTap: { scale: 0.95, transition: { duration: 0.1 } },
+  whileHover: { scale: 1.1, transition: { duration: 0.1 } },
+  transition: {
+    type: 'spring',
+    stiffness: 100,
+    damping: 10,
+    mass: 1,
+  },
+})<NavItemProps>`
+  margin: 10px;
+  &:hover {
+    cursor: pointer;
+    font-weight: bold;
+  }
+  @media (max-width: 990px) {
+    display: ${({ hide }) => hide && 'none'};
+  }
+`;
+
+export const NavContent = styled(Container)``;
