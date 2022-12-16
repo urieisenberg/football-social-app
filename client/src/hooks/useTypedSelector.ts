9a48ac03cf64cfa34c87b58352bacb7e930ba732
urieisenberg <urieisenberg@gmail.com>
2022-12-16 22:25:21 +0200

    removing that to  useAppSelector

--- client/src/hooks/useTypedSelector.ts
+++
@@ -1,4 +0,0 @@
-import { useSelector, TypedUseSelectorHook } from 'react-redux';
-import { RootState } from '../app/store';
-
-export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
