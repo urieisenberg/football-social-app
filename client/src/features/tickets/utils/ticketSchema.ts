d4e2186d633147f91593f5b7269d41b9c23c5c6f
urieisenberg <urieisenberg@gmail.com>
2022-12-18 00:49:42 +0200

    fix min char to match server model

--- client/src/features/tickets/utils/ticketSchema.ts
+++ client/src/features/tickets/utils/ticketSchema.ts
@@ -6,6 +6,6 @@ export const ticketSchema = Yup.object().shape({
     .required('Subject is required'),
   message: Yup.string()
     .required('Description is required')
-    .min(5, 'Description must be at least 5 characters')
+    .min(10, 'Description must be at least 10 characters')
     .max(50, 'Description must be less than 50 characters'),
 });
