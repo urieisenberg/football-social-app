a2cb7c32fb5ca236eb0adae4dd0dde356782dcd1
urieisenberg <urieisenberg@gmail.com>
2022-12-18 15:29:33 +0200

    exporting update

--- server/src/validators/index.ts
+++ server/src/validators/index.ts
@@ -2,3 +2,5 @@ export * from './registerSchema';
 export * from './loginSchema';
 export * from './noteSchema';
 export * from './ticketSchema';
+export * from './postSchema';
+export * from './commentSchema';
