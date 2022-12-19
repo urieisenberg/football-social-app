58e4b9e4563d6043fea11e720b48309c557c6f24
urieisenberg <urieisenberg@gmail.com>
2022-12-19 13:47:02 +0200

    rendering notes list

---
+++ client/src/features/notes/List/index.tsx
@@ -0,0 +1,51 @@
+import {
+  useGetNotesQuery,
+  useDeleteNoteMutation,
+} from '../../../app/services/server-api/notes';
+import { Transition } from '../../../components/Transition';
+import { NotesContainer, NotesContent } from '../styles';
+import { DeleteNote } from '../../../app/types';
+import { NotesItem } from '../Item';
+
+interface NotesProps {
+  ticketId: string;
+}
+
+export const Notes = ({ ticketId }: NotesProps) => {
+  const { data } = useGetNotesQuery({
+    ticketId: ticketId,
+  });
+
+  const [deleteNote] = useDeleteNoteMutation();
+
+  const onDelete = async ({ noteId, ticketId }: DeleteNote) => {
+    deleteNote({
+      noteId: noteId,
+      ticketId: ticketId,
+    });
+  };
+
+  console.log(data)
+
+  let content;
+  if (data?.length)
+    content = (
+      <Transition key="notes">
+        <NotesContainer>
+          <NotesContent>
+            {data?.map((note) => (
+              <NotesItem
+                key={note._id}
+                note={note}
+                onDelete={onDelete}
+                ticketId={ticketId}
+              />
+            ))}
+          </NotesContent>
+        </NotesContainer>
+      </Transition>
+    );
+  else content = null;
+
+  return content;
+};
