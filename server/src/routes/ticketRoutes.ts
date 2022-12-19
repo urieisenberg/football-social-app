9d8bcdb4b0cad19485a5c864e89ef1fb71e8fbee
urieisenberg <urieisenberg@gmail.com>
2022-12-19 12:54:44 +0200

    add close ticket route

--- server/src/routes/ticketRoutes.ts
+++ server/src/routes/ticketRoutes.ts
@@ -5,6 +5,7 @@ import {
   deleteTicket,
   getTicketById,
   getTickets,
+  closeTicket,
 } from '../controllers/ticketController';
 import { protect } from '../middleware/authMiddleware';
 import { noteRoutes } from './noteRoutes';
@@ -14,10 +15,13 @@ const router = Router();
 router.use('/:ticketId/notes', noteRoutes);
 
 router.route('/').post(protect, createTicket).get(protect, getTickets);
+
 router
   .route('/:id')
   .put(protect, updateTicket)
   .delete(protect, deleteTicket)
   .get(protect, getTicketById);
+
+router.route('/:id/close').put(protect, closeTicket);
 
 export { router as ticketRoutes };
