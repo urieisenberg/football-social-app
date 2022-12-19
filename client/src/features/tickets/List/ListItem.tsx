0b95dbc2f3d7ecdc396146388e81c14c248a94b0
urieisenberg <urieisenberg@gmail.com>
2022-12-19 12:55:32 +0200

    onDelete ticket now here

--- client/src/features/tickets/List/ListItem.tsx
+++ client/src/features/tickets/List/ListItem.tsx
@@ -1,13 +1,43 @@
-import { Link } from 'react-router-dom';
-import { Button } from '../../../components/Button';
+import { useNavigate } from 'react-router-dom';
+import { useDeleteTicketMutation } from '../../../app/services/server-api/tickets';
+import { DeleteTicket } from '../../../app/types';
 import { Ticket } from '../../../app/types';
+import { toast } from 'react-toastify';
 import { ItemContainer, ItemHeading, ItemStatus } from '../styles';
+import { Button } from '../../../components/Button';
 
 interface Props {
   ticket: Ticket;
 }
 
 export const TicketsListItem = ({ ticket }: Props) => {
+  const navigate = useNavigate();
+
+  const [deleteTicket] = useDeleteTicketMutation();
+
+  const onDelete = async (data: DeleteTicket) => {
+    await deleteTicket(data);
+    toast.success('Ticket deleted');
+    navigate('/contact/tickets');
+  };
+
+  let actionContent;
+  ticket.status === 'open'
+    ? (actionContent = (
+        <Button
+          text="View"
+          variant="primary"
+          onClick={() => navigate(`/contact/tickets/${ticket?._id}`)}
+        />
+      ))
+    : (actionContent = (
+        <Button
+          text="delete"
+          variant="error"
+          onClick={() => onDelete({ id: ticket?._id as number })}
+        />
+      ));
+
   return (
     <ItemContainer>
       <ItemHeading>
@@ -15,9 +45,7 @@ export const TicketsListItem = ({ ticket }: Props) => {
       </ItemHeading>
       <ItemHeading>{ticket?.subject}</ItemHeading>
       <ItemStatus status={ticket?.status}> {ticket?.status}</ItemStatus>
-      <Link to={`/contact/tickets/${ticket?._id}`}>
-        <Button text="View" />
-      </Link>
+      {actionContent}
     </ItemContainer>
   );
 };
