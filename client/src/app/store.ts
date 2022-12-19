e1eb99977f610f98b4c112c136e891fdfb9cc18d
urieisenberg <urieisenberg@gmail.com>
2022-12-19 13:40:12 +0200

    noteReducer added

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,15 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query/react';
-import {api} from './services/api';
-import { authApi } from './services/auth';
+import { api } from './services/server-api';
+import { authApi } from './services/server-api/auth';
 import authReducer from '../features/auth/authSlice';
 import ticketReducer from '../features/tickets/ticketsSlice';
+import noteReducer from '../features/notes/noteSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     [authApi.reducerPath]: authApi.reducer,
+    [api.reducerPath]: api.reducer,
     ticket: ticketReducer,
+    note: noteReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
@@ -20,4 +23,3 @@ setupListeners(store.dispatch);
 export type RootState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
-
