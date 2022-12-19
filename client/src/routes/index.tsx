969daaff174a447ca2d72ce3f94ac9d728fdfd8a
urieisenberg <urieisenberg@gmail.com>
2022-12-19 13:40:00 +0200

    routes init

---
+++ client/src/routes/index.tsx
@@ -0,0 +1,35 @@
+import {
+  Routes as Routing,
+  Route,
+  Navigate,
+  useLocation,
+} from 'react-router-dom';
+import { Protected } from './Protected';
+import { Public } from './Public';
+import { Welcome } from '../pages/Welcome';
+import { Register } from '../features/auth/Register';
+import { Login } from '../features/auth/Login';
+import { NotFound } from '../pages/NotFound';
+import { Contact } from '../pages/Contact';
+
+export const Routes = () => {
+  const location = useLocation();
+  return (
+    <Routing location={location} key={location.pathname}>
+      {/* Protected Routes */}
+      <Route path="/" element={<Protected />}>
+        <Route path="" element={<Navigate to="league/135" />} />
+        {/* <Route path="league/:leagueid/*" element={<League />} /> */}
+        <Route path="contact/*" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+      {/* Public Routes */}
+      <Route path="welcome" element={<Public />}>
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/welcome" />} />
+      </Route>
+    </Routing>
+  );
+};
