4bd37864eaec9ed11ff848e64ae69d25f8a8952b
urieisenberg <urieisenberg@gmail.com>
2022-12-20 20:59:01 +0200

    showing options

--- client/src/features/posts/Item/index.tsx
+++ client/src/features/posts/Item/index.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import { useToggle } from '../../../hooks/useToggle';
 import { Post } from '../../../app/types';
 import { LikePost } from './LikePost';
+import { PostOptions } from './PostOptions';
 import {
   Wrapper,
   Container,
@@ -20,9 +21,10 @@ interface PostItemProps {
 export const PostItem = ({ post }: PostItemProps) => {
   const navigate = useNavigate();
 
+  const [showEdit, toggleEdit] = useToggle(false);
+
   const createdAt = new Date(post.createdAt).toLocaleString();
-  const updatedAt =
-    post.updatedAt && new Date(post.updatedAt).toLocaleString();
+  const updatedAt = post.updatedAt && new Date(post.updatedAt).toLocaleString();
 
   return (
     <Wrapper>
@@ -38,8 +40,12 @@ export const PostItem = ({ post }: PostItemProps) => {
           </Name>
           {/* Follow */}
           <CreatedAt>{createdAt}</CreatedAt>
-          {/* Actions */}
-          <Text>{post.text}</Text>
+          <PostOptions
+            post={post}
+            showEdit={showEdit}
+            toggleEdit={toggleEdit}
+          />
+          <Text>{!showEdit && post.text}</Text>
           <LikePost post={post} />
           {updatedAt !== createdAt && (
             <UpdatedAt>post last updated: {updatedAt}</UpdatedAt>
