9eed36102f87565766892311c6355f8242284ef9
urieisenberg <urieisenberg@gmail.com>
2022-12-20 20:08:35 +0200

    style for post content

---
+++ client/src/features/posts/Item/styles/Content.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 650px;
+  height: 100%;
+  @media only screen and (max-width: 750px) {
+    font-size: 0.9rem;
+    width: 300px;
+    height: 100%;
+  }
+  display: flex;
+  flex-direction: column;
+  width: 650px;
+  height: 100%;
+  @media only screen and (max-width: 750px) {
+    font-size: 0.9rem;
+    width: 300px;
+    height: 100%;
+  }
+`;
