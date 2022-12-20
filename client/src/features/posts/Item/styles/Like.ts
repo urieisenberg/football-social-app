bdcaf2ab9862361188a227db5d64daeb505d206b
urieisenberg <urieisenberg@gmail.com>
2022-12-20 16:02:33 +0200

    styling like post

---
+++ client/src/features/posts/Item/styles/Like.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Like = styled.div`
+  position: absolute;
+  right: 40px;
+  top: 80px;
+  @media only screen and (max-width: 768px) {
+    top: 75px;
+    font-size: 1rem;
+  }
+`;
+
+export const LikeCount = styled.span`
+  margin-right: 0.5rem;
+  font-size: 0.7rem;
+`;
+
+export const LikeIcon = styled.span`
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.05);
+    font-weight: 600;
+    color: ${({ theme }) => theme.primary};
+  }
+`;
