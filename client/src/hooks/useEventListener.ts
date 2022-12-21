33939cda5be0046ef946be3784f5d94ed42a439e
urieisenberg <urieisenberg@gmail.com>
2022-12-21 18:09:44 +0200

    document as default elem

--- client/src/hooks/useEventListener.ts
+++ client/src/hooks/useEventListener.ts
@@ -1,9 +1,9 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef } from 'react';
 
 export const useEventListener = (
   eventType: string,
   handler: any,
-  element = window
+  element = document
 ) => {
   const handlerRef = useRef(handler);
 
