e817e647cf2d5f09d79ba22e8118c6b2f4228ce6
urieisenberg <urieisenberg@gmail.com>
2022-12-21 15:08:38 +0200

    clean

--- client/src/features/posts/Item/styles/Content.ts
+++ client/src/features/posts/Item/styles/Content.ts
@@ -10,13 +10,4 @@ export const Content = styled.div`
     width: 300px;
     height: 100%;
   }
-  display: flex;
-  flex-direction: column;
-  width: 650px;
-  height: 100%;
-  @media only screen and (max-width: 750px) {
-    font-size: 0.9rem;
-    width: 300px;
-    height: 100%;
-  }
 `;
