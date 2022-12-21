f495d87d8ef33a7fa4d0308769e87b26fab5e603
urieisenberg <urieisenberg@gmail.com>
2022-12-21 15:02:54 +0200

    handle comments too plus reset input after submit

--- client/src/components/Form/Share/index.tsx
+++ client/src/components/Form/Share/index.tsx
@@ -1,44 +1,71 @@
+import { useEffect } from 'react';
 import { useFormContext, Controller, FieldError } from 'react-hook-form';
 import {
   FormShareContainer,
   FormShareButton,
   FormShareInput,
   FormShareError,
+  FormShareAltInput,
+  FormShareSubmit,
 } from '../styles';
 import { RiShareForwardFill, RiShareForwardLine } from 'react-icons/ri';
 
 interface ShareProps {
   type: string;
   errors: FieldError;
+  title: string;
   placeholder?: string;
 }
 
-export const Share = ({ type, placeholder, errors }: ShareProps) => {
-  const { control } = useFormContext();
+export const Share = ({ type, placeholder, errors, title }: ShareProps) => {
+  const { control, reset, formState } = useFormContext();
+
+  useEffect(() => {
+    if (formState.isSubmitted) reset();
+  }, [formState.isSubmitted, reset]);
 
   return (
     <>
       <Controller
-        name={type}
+        name={title.includes('post') ? 'text' : 'comment'}
         control={control}
         defaultValue=""
         render={({ field }) => (
-          <FormShareContainer>
-            <FormShareInput
-              {...field}
-              type={type}
-              onBlur={field.onBlur}
-              onChange={field.onChange}
-              placeholder={placeholder}
-              className={` ${errors && 'is-invalid'}`}
-            />
-            <FormShareButton type="submit">
-              {errors ? <RiShareForwardLine /> : <RiShareForwardFill />}
-            </FormShareButton>{' '}
-            {errors && <FormShareError>{errors.message}</FormShareError>}
-          </FormShareContainer>
+          <>
+            {title.includes('post') ? (
+              <FormShareContainer>
+                <FormShareInput
+                  {...field}
+                  type={type}
+                  onBlur={field.onBlur}
+                  onChange={field.onChange}
+                  placeholder={placeholder}
+                  className={` ${errors && 'is-invalid'}`}
+                />
+                <FormShareButton type="submit">
+                  {errors ? <RiShareForwardLine /> : <RiShareForwardFill />}
+                </FormShareButton>{' '}
+                {errors && <FormShareError>{errors.message}</FormShareError>}
+              </FormShareContainer>
+            ) : (
+              <>
+                <FormShareAltInput
+                  autoFocus
+                  {...field}
+                  type={title}
+                  onBlur={field.onBlur}
+                  onChange={field.onChange}
+                  placeholder={placeholder}
+                />
+                <FormShareSubmit type="submit">
+                  {errors ? <RiShareForwardLine /> : <RiShareForwardFill />}
+                </FormShareSubmit>
+                {errors && <FormShareError>{errors.message}</FormShareError>}
+              </>
+            )}
+          </>
         )}
-      />{' '}
+      />
     </>
   );
 };
