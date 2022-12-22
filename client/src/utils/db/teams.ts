26cb28a0f7e590760fe4da3830f73f182f3a7dc6
urieisenberg <urieisenberg@gmail.com>
2022-12-22 18:35:08 +0200

    typing

--- client/src/utils/db/teams.ts
+++ client/src/utils/db/teams.ts
@@ -1,116 +1,115 @@
-import * as logos from "../images/logos";
+import { Team } from '../../app/types';
+import * as logos from '../images/logos';
 
-export const teams = [
+export const teams: Team[] = [
   {
-    name: "AC Milan",
+    name: 'AC Milan',
     id: 489,
     logo: logos.milan,
   },
   {
-    name: "Inter Milan",
+    name: 'Inter Milan',
     id: 505,
     logo: logos.inter,
   },
   {
-    name: "Napoli",
+    name: 'Napoli',
     id: 492,
     logo: logos.napoli,
   },
   {
-    name: "Juventus",
+    name: 'Juventus',
     id: 496,
     logo: logos.juventus,
   },
   {
-    name: "Lazio",
+    name: 'Lazio',
     id: 487,
     logo: logos.lazio,
   },
   {
-    name: "Roma",
+    name: 'Roma',
     id: 497,
     logo: logos.roma,
   },
   {
-    name: "Fiorentina",
+    name: 'Fiorentina',
     id: 502,
     logo: logos.fiorentina,
   },
   {
-    name: "Atalanta",
+    name: 'Atalanta',
     id: 499,
     logo: logos.atalanta,
   },
   {
-    name: "Hellas Verona",
+    name: 'Hellas Verona',
     id: 504,
     logo: logos.hellasVerona,
   },
   {
-    name: "Torino",
+    name: 'Torino',
     id: 503,
     logo: logos.torino,
   },
   {
-    name: "Sassuolo",
+    name: 'Sassuolo',
     id: 488,
     logo: logos.sassuolo,
   },
   {
-    name: "Udinese",
+    name: 'Udinese',
     id: 494,
     logo: logos.udinese,
   },
   {
-    name: "Bologna",
+    name: 'Bologna',
     id: 500,
     logo: logos.bolonga,
   },
   {
-    name: "Empoli",
+    name: 'Empoli',
     id: 511,
     logo: logos.empoli,
   },
   {
-    name: "Sampdoria",
+    name: 'Sampdoria',
     id: 498,
     logo: logos.sampdoria,
   },
   {
-    name: "Spezia",
+    name: 'Spezia',
     id: 515,
     logo: logos.spezia,
   },
   {
-    name: "Salernitana",
+    name: 'Salernitana',
     id: 514,
     logo: logos.salernitana,
   },
   {
-    name: "Cagliari",
+    name: 'Cagliari',
     id: 490,
     logo: logos.cagliari,
   },
   {
-    name: "Genoa",
+    name: 'Genoa',
     id: 495,
     logo: logos.genoa,
   },
   {
-    name: "Cremonese",
+    name: 'Cremonese',
     id: 520,
     logo: logos.cremonese,
   },
   {
-    name: "Lecce",
+    name: 'Lecce',
     id: 867,
     logo: logos.lecce,
   },
   {
-    name: "Monza",
+    name: 'Monza',
     id: 1579,
     logo: logos.monza,
   },
 ];
-
-
