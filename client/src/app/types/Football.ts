a222223a8129327020795b8d0f46328a293145c3
urieisenberg <urieisenberg@gmail.com>
2022-12-22 17:36:36 +0200

    football init type

--- client/src/app/types/Football.ts
+++ client/src/app/types/Football.ts
@@ -1,6 +1,22 @@
+export const SERIA_A = '135';
+
 export interface Venue {
-  id: number;
+  id: string;
   country: string;
   capacity: number;
   searchTerm?: string;
+}
+
+export interface LeagueById {
+  leagueid: string;
+}
+
+export interface LeagueByTeam {
+  teamid: string;
+}
+
+export interface Team {
+  id: number;
+  name: string;
+  logo: string;
 }
