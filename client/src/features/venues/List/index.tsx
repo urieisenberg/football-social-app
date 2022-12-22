e19ee30d6695c14c64d323396874604751d254c3
urieisenberg <urieisenberg@gmail.com>
2022-12-22 21:35:54 +0200

    rendering venues list

---
+++ client/src/features/venues/List/index.tsx
@@ -0,0 +1,49 @@
+import { useParams, useNavigate } from 'react-router-dom';
+import { useGetVenuesQuery } from '../../../app/services/football-api/veneus';
+import { useGetLeagueInfoQuery } from '../../../app/services/football-api/leagues';
+import {
+  VenuesContainer,
+  VenuesTitle,
+  VenuesListContainer,
+  VenuesImage,
+  VenuesName,
+  VenuesItemContainer,
+} from '../style';
+import { Loader } from '../../../components/Loader';
+import { Transition } from '../../../components/Transition';
+import { TopButton } from '../../../components/Button';
+
+export const VenuesList = () => {
+  const navigate = useNavigate();
+  const { leagueid } = useParams();
+  const { data: league } = useGetLeagueInfoQuery(leagueid as string);
+  const country = league?.country.name as string;
+  const { data: venues, isLoading, isSuccess } = useGetVenuesQuery(country);
+
+  let content;
+  if (isLoading) content = <Loader />;
+  else if (isSuccess && venues)
+    content = (
+      <Transition key="venues">
+        <VenuesContainer>
+          <VenuesTitle>Venues From {country}</VenuesTitle>
+          <VenuesListContainer>
+            {venues.map((venue) => (
+              <VenuesItemContainer
+                key={venue.id}
+                onClick={() =>
+                  navigate(`/league/${leagueid}/venues/${venue.id}`)
+                }
+              >
+                <VenuesImage src={venue.image} alt={venue.name} />
+                <VenuesName>{venue.name}</VenuesName>
+              </VenuesItemContainer>
+            ))}
+          </VenuesListContainer><TopButton />
+        </VenuesContainer>
+        
+      </Transition>
+    );
+
+  return <section>{content}</section>;
+};
