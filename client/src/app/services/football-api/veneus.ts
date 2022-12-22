3eb60c5f7c7eaf39dd78bdb9601f8b5a731b1d58
urieisenberg <urieisenberg@gmail.com>
2022-12-22 21:20:32 +0200

    import transformResponses

--- client/src/app/services/football-api/veneus.ts
+++ client/src/app/services/football-api/veneus.ts
@@ -1,26 +1,34 @@
 import { footballApi } from '.';
 import { Venue } from '../../types';
+import {
+  transformVenueItemResponse,
+  transformVenuesListResponse,
+  transformVenuesSearchedResponse,
+} from './transformResponses';
 
 const URL = 'venues';
 
 export const venuesApi = footballApi.injectEndpoints({
   endpoints: (builder) => ({
-    getVenues: builder.query<void, Venue>({
-      query: ({ country }) => `${URL}?country=${country}`,
-      providesTags: ['Venue'],
-      transformResponse: (response, meta, arg) => {
-        const { data } = response as { data: Venue[] };
-        data.filter((venue) => venue.capacity > 10000);
+    getVenues: builder.query<Venue[], string>({
+      query: (country) => `${URL}?country=${country}`,
+      transformResponse: transformVenuesListResponse,
+      forceRefetch({ currentArg, previousArg }) {
+        return currentArg !== previousArg;
       },
     }),
-    getVenue: builder.query<void, Venue>({
+    getVenue: builder.query<Venue, Venue>({
       query: ({ id }) => `${URL}?id=${id}`,
+      transformResponse: transformVenueItemResponse,
+      forceRefetch({ currentArg, previousArg }) {
+        return currentArg !== previousArg;
+      },
     }),
-    searchVenues: builder.query<void, Venue>({
-      query: ({ searchTerm }) => `${URL}?search=${searchTerm}`,
-      transformResponse: (response, meta, arg) => {
-        const { data } = response as { data: Venue[] };
-        data.filter((venue) => venue.country === 'Italy');
+    searchVenues: builder.query<Venue[], string>({
+      query: (searchTerm) => `${URL}?search=${searchTerm}`,
+      transformResponse: transformVenuesSearchedResponse,
+      forceRefetch({ currentArg, previousArg }) {
+        return currentArg !== previousArg;
       },
     }),
   }),
