210c8a7773969c8144a27678f62b424869ca9f6b
urieisenberg <urieisenberg@gmail.com>
2022-12-22 17:35:45 +0200

    football api middleware and userReducer added

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,25 +1,29 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query/react';
 import { api } from './services/server-api';
+import { footballApi } from './services/football-api';
 import { authApi } from './services/server-api/auth';
-import authReducer from '../features/auth/authSlice';
-import ticketReducer from '../features/tickets/ticketSlice';
-import noteReducer from '../features/notes/noteSlice';
-import postReducer from '../features/posts/postSlice';
-import commentReducer from '../features/comments/commentSlice';
+import authReducer from './actions/authSlice';
+import ticketReducer from './actions/ticketSlice';
+import noteReducer from './actions/noteSlice';
+import postReducer from './actions/postSlice';
+import commentReducer from './actions/commentSlice';
+import userReducer from './actions/userSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     [authApi.reducerPath]: authApi.reducer,
     [api.reducerPath]: api.reducer,
+    [footballApi.reducerPath]: footballApi.reducer,
     ticket: ticketReducer,
     note: noteReducer,
     post: postReducer,
     comment: commentReducer,
+    user: userReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(api.middleware),
+    getDefaultMiddleware().concat(api.middleware, footballApi.middleware),
 });
 
 setupListeners(store.dispatch);
