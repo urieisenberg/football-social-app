76665c02938b0f00c12b8b19ba19f0c8bc94eeb1
urieisenberg <urieisenberg@gmail.com>
2022-12-23 20:18:02 +0200

    transformResponse for team calls

--- client/src/features/teams/utils/teamTransformResponse.ts
+++ client/src/features/teams/utils/teamTransformResponse.ts
@@ -3,3 +3,7 @@ import { TeamInfo } from '../types';
 export const transformTeamInfoResponse = (response: any) => {
   return response.response[0] as TeamInfo;
 };
+
+export const transformTeamStatisticsResponse = (response: any) => {
+  return response.response;
+};
