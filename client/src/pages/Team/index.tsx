87d21c8edd74ea13781d385bdc074c263fefe616
urieisenberg <urieisenberg@gmail.com>
2022-12-23 21:23:34 +0200

    init team page

---
+++ client/src/pages/Team/index.tsx
@@ -0,0 +1,48 @@
+import { useParams, useRoutes, Outlet, Navigate } from 'react-router-dom';
+import { teams } from '../../utils/db/teams';
+import { TeamInformation } from '../../features/teams/Info';
+import { VenuesItem } from '../../features/venues/Item';
+import { SocialLayout } from '../../features/social/Layout';
+import { PlayersLayout } from '../../features/players/Layout';
+import { Standings } from '../../features/standings';
+import { Nav } from '../../components/Nav';
+
+export const Team = () => {
+  const { teamid } = useParams();
+
+  let elements = useRoutes([
+    { path: '', element: <Navigate to="fixtures" /> },
+    { path: 'fixtures/*', element: <>Fixtures</> },
+    { path: 'players/*', element: <PlayersLayout /> },
+    { path: 'social/*', element: <SocialLayout /> },
+    { path: 'standings/', element: <Standings /> },
+    // { path: "coach", element: <CoachLayout /> },
+    // { path: "transfers/*", element: <TransfersLayout /> },
+    { path: 'stadium', element: <VenuesItem /> },
+    // {path:'feed', element: <>Feed</>},
+  ]);
+
+  const currentTeam = teams.find((team) => team.id.toString() === teamid);
+
+  const renderNavLinks = () => {
+    let links = [
+      'fixtures',
+      'players',
+      'transfers',
+      'standings',
+      'stadium',
+      'coach',
+    ];
+    if (currentTeam) links.push('social', 'feed');
+    return links;
+  };
+
+  return (
+    <>
+      <TeamInformation />
+      <Nav links={renderNavLinks()} />
+      <Outlet />
+      {elements}
+    </>
+  );
+};
