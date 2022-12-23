b3ee1bf7289676a9f797e9ff28967b5a223ae6c7
urieisenberg <urieisenberg@gmail.com>
2022-12-23 03:07:14 +0200

    exporting coachs, standings and trophies type

--- client/src/app/types/index.ts
+++ client/src/app/types/index.ts
@@ -3,11 +3,12 @@ export * from '../../features/tickets/types';
 export * from '../../features/notes/types';
 export * from '../../features/posts/types';
 export * from '../../features/comments/types';
-export * from '../../features/coaches/types';
+export * from '../../features/coachs/types';
 export * from '../../features/players/types';
 export * from '../../features/team/types';
 export * from '../../features/venues/types';
 export * from '../../features/leagues/types';
 export * from '../../features/fixtures/types';
 export * from '../../features/injuries/types';
-
+export * from '../../features/standings/types';
+export * from '../../features/trophies/types';
