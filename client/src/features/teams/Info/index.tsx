a8272e971726215929382900f224e0cf2faffaa1
urieisenberg <urieisenberg@gmail.com>
2022-12-23 20:31:11 +0200

    team info comp

---
+++ client/src/features/teams/Info/index.tsx
@@ -0,0 +1,37 @@
+import { useParams } from 'react-router-dom';
+import { Transition } from '../../../components/Transition';
+import { useGetTeamInfoQuery } from '../api';
+import {
+  InfoWrapper,
+  InfoContainer,
+  InfoTitle,
+  InfoLogo,
+  InfoCountry,
+} from '../styles';
+
+export const TeamInformation = () => {
+  const { teamid } = useParams();
+
+  const { data, isLoading, isError } = useGetTeamInfoQuery(teamid as string);
+
+  let content;
+  if (isLoading || isError) content = null;
+  else
+    content = (
+      <Transition key={data?.team.id}>
+        <InfoWrapper>
+          <InfoContainer>
+            <InfoLogo src={data?.team.logo} alt={data?.team.name} />
+            <InfoTitle>
+              {data?.team.name + ' since ' + data?.team?.founded}
+            </InfoTitle>
+            <InfoCountry>
+              {data?.venue?.city + ', ' + data?.venue?.country}
+            </InfoCountry>
+          </InfoContainer>
+        </InfoWrapper>
+      </Transition>
+    );
+
+  return content;
+};
