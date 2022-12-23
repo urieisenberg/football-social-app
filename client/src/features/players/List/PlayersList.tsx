222f86602599aaf895d62b3d082b2cfc7b3792a6
urieisenberg <urieisenberg@gmail.com>
2022-12-23 19:43:37 +0200

    reusable comp to render players list

---
+++ client/src/features/players/List/PlayersList.tsx
@@ -0,0 +1,61 @@
+import { usePathname } from '../../../hooks/usePathname';
+import { Loader } from '../../../components/Loader';
+import { TopButton } from '../../../components/Button';
+import { Card } from '../../../components/Card';
+import {
+  PlayersContainer,
+  PlayersTitle,
+  PlayersListContainer,
+} from '../styles';
+import { PlayerStatistics } from '../types';
+
+interface PlayersListProps {
+  data: PlayerStatistics[];
+  title: string;
+  isLoading: boolean;
+}
+
+export const PlayersList = ({ data, title, isLoading }: PlayersListProps) => {
+  const { pathMatch } = usePathname();
+
+  let content;
+  if (isLoading) content = <Loader />;
+  else if (data.length === 0)
+    content = (
+      <PlayersContainer>
+        <PlayersTitle>{title}</PlayersTitle>
+        No Players Found
+      </PlayersContainer>
+    );
+  else
+    content = (
+      <PlayersContainer>
+        <PlayersTitle>{title}</PlayersTitle>
+        <PlayersListContainer>
+          {data.map((player) => (
+            <Card
+              key={player.player.id}
+              player={player}
+              image={player.player.photo}
+              name={player.player.name}
+              team={player.statistics[0].team}
+              type={
+                title.includes('Red')
+                  ? player.statistics[0].cards.red + ' 🟥'
+                  : title.includes('Yellow')
+                  ? player.statistics[0].cards.yellow + ' 🟨'
+                  : title.includes('Scorers')
+                  ? player.statistics[0].goals.total + ' Goals'
+                  : title.includes('Assists')
+                  ? player.statistics[0].goals.assists + ' Assists'
+                  : ''
+              }
+            />
+          ))}
+        </PlayersListContainer>
+        {!pathMatch('searchTerm', 'includes') && <TopButton />}
+      </PlayersContainer>
+    );
+
+  return <section>{content}</section>;
+};
