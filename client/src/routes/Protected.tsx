e9bf2b473610181b1508302730357d1dfe41f92e
urieisenberg <urieisenberg@gmail.com>
2022-12-23 21:23:16 +0200

    back button

--- client/src/routes/Protected.tsx
+++ client/src/routes/Protected.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { Sidebar } from '../components/Sidebar';
 import { useAuth } from '../hooks/useAuth';
+import { BackButton } from '../components/Button';
 
 export const Protected = () => {
   const { isAuthenticated } = useAuth();
@@ -9,6 +10,7 @@ export const Protected = () => {
 
   return (
     <>
+      <BackButton />
       <Sidebar />
       <Outlet />
     </>
