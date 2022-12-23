76a4df92260aebdf2b1d07feee8b532a505f8d21
urieisenberg <urieisenberg@gmail.com>
2022-12-23 23:15:44 +0200

    styling injuries list

---
+++ client/src/features/injuries/styles/index.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const InjuriesContainer = styled.div.attrs({
+  className: 'container text-center justify-content-center align-items-center',
+})`
+  overflow: hidden;
+`;
+
+export const InjuriesPlayers = styled.div.attrs({
+  className: 'row row-cols-auto gx-3',
+})``;
+
+export const InjuriesDates = styled.span.attrs({
+    className: 'text-muted',
+  })``;
+  
