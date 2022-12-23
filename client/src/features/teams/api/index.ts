b1a875b5048a2531fb238a10c1e618636b676542
urieisenberg <urieisenberg@gmail.com>
2022-12-23 20:17:30 +0200

    getTeamStats api call

--- client/src/features/teams/api/index.ts
+++ client/src/features/teams/api/index.ts
@@ -1,6 +1,9 @@
 import { footballApi } from '../../../app/services/football-api';
-import { TeamInfo } from '../types';
-import { transformTeamInfoResponse } from '../utils/teamTransformResponse';
+import { TeamInfo, TeamStatistics } from '../types';
+import {
+  transformTeamInfoResponse,
+  transformTeamStatisticsResponse,
+} from '../utils/teamTransformResponse';
 
 export const teamsApi = footballApi.injectEndpoints({
   endpoints: (builder) => ({
@@ -11,7 +14,18 @@ export const teamsApi = footballApi.injectEndpoints({
         return currentArg !== previousArg;
       },
     }),
+    getTeamStatistics: builder.query<
+      TeamStatistics[],
+      { teamid: string; leagueid: string }
+    >({
+      query: ({ teamid, leagueid }) =>
+        `teams/statistics?league=${leagueid}&id=${teamid}&season=2022`,
+      transformResponse: transformTeamStatisticsResponse,
+      forceRefetch({ currentArg, previousArg }) {
+        return currentArg !== previousArg;
+      },
+    }),
   }),
 });
 
-export const { useGetTeamInfoQuery } = teamsApi;
+export const { useGetTeamInfoQuery, useGetTeamStatisticsQuery } = teamsApi;
