5ac7e528bd4df82e421c0d750e36086f8e2567f6
urieisenberg <urieisenberg@gmail.com>
2022-12-23 03:06:13 +0200

    remove table

--- client/src/features/leagues/utils/leagueTransformeResponse.ts
+++ client/src/features/leagues/utils/leagueTransformeResponse.ts
@@ -8,9 +8,3 @@ export const transformCurrentLeagueResponse = (response: any) => {
   return response.response[0].league.id;
 };
 
-export const transformLeagueTableResponse = (response: any) => {
-  return {
-    league: response.response[0].league,
-    standings: response.response[0].league.standings[0],
-  };
-};
