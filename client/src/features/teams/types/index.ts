81aa2d90e95e4c3b8a3cc007a407ab4721c5a7c3
urieisenberg <urieisenberg@gmail.com>
2022-12-23 20:18:17 +0200

    export

--- client/src/features/teams/types/index.ts
+++ client/src/features/teams/types/index.ts
@@ -1,24 +1,3 @@
-export interface Team {
-    id: number;
-    name: string;
-    logo: string;
-  }
-  
-  export interface TeamInfo {
-    team: {
-      id: number;
-      name: string;
-      logo: string;
-      founded: number;
-      country: string;
-    };
-    venue: {
-      id: number;
-      name: string;
-      address: string;
-      city: string;
-      capacity: number;
-      image: string;
-    };
-  }
-  
+export * from './Info';
+export * from './Team';
+export * from './Statistics';
