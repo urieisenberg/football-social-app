d841aca81b8feafc7cbee9b31176e27a36d640f2
urieisenberg <urieisenberg@gmail.com>
2022-12-24 00:55:32 +0200

    fix

--- client/src/features/injuries/utils/injuriesReasonsHandler.ts
+++ client/src/features/injuries/utils/injuriesReasonsHandler.ts
@@ -1,19 +1,14 @@
-import { ReasonOfInjury } from '../types';
-
-export const injuriesReasonsHandler = (reasons: ReasonOfInjury) => {
-  const reasonsOfInjuries = reasons.map((reason) => {
-    switch (reason) {
-      case 'Injury':
-        return 'Injury 🚑';
-      case 'Red Card':
-        return 'Red Card 🟥';
-      case 'Suspended':
-        return 'Suspended 🔴';
-      case 'Yellow Cards':
-        return 'Yellow Cards 🟨🟨';
-      default:
-        return 'injured';
-    }
-  });
-  return reasonsOfInjuries;
+export const injuriesReasonsHandler = (reason: string) => {
+  switch (reason) {
+    case 'Injury':
+      return 'Injury 🚑';
+    case 'Red Card':
+      return 'Red Card 🟥';
+    case 'Suspended':
+      return 'Suspended 🔴';
+    case 'Yellow Cards':
+      return 'Yellow Cards 🟨🟨';
+    default:
+      return reason;
+  }
 };
