a989b618e32941ee8ecdbab49e440886b760edef
urieisenberg <urieisenberg@gmail.com>
2022-12-24 21:52:42 +0200

    changes

--- client/src/routes/hooks/useNavigateToPlayer.ts
+++ client/src/routes/hooks/useNavigateToPlayer.ts
@@ -1,11 +1,15 @@
 import { useNavigate } from 'react-router-dom';
 
-export const useNavigateToPlayer = (
-  teamid: string,
-  teamname: string,
-  playerid: string
-) => {
+export const useNavigateToPlayer = () => {
   const navigate = useNavigate();
-  return () => navigate(`/team/${teamid}/player/${playerid}`);
-};
 
+  const navigateToPlayer = (
+    teamid: number,
+    teamname: string,
+    playerid: number
+  ) => {
+    navigate(`/team/${teamid}/${teamname}/players/${playerid}`);
+  };
+
+  return { navigateToPlayer };
+};
