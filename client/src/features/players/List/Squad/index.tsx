9a457eecffb7e70bbc0b735a6e05851845f7cb0a
urieisenberg <urieisenberg@gmail.com>
2022-12-24 21:52:05 +0200

    useNavigateToPlayer for card props

--- client/src/features/players/List/Squad/index.tsx
+++ client/src/features/players/List/Squad/index.tsx
@@ -1,13 +1,15 @@
 import { useParams } from 'react-router-dom';
 import { useGetSquadQuery } from '../../api';
+import { useNavigateToPlayer } from '../../../../app/hooks';
 import { Loader } from '../../../../components/Loader';
 import { TopButton } from '../../../../components/Button';
 import { Card } from '../../../../components/Card';
 import { PlayersContainer, PlayersListContainer } from '../../styles';
 
 export const SquadPlayersList = () => {
-  const { teamid } = useParams();
+  const { teamid, teamname } = useParams();
   const { data, isLoading, isSuccess } = useGetSquadQuery(teamid as string);
+  const { navigateToPlayer } = useNavigateToPlayer();
 
   let content;
   if (isLoading) content = <Loader />;
@@ -22,6 +24,13 @@ export const SquadPlayersList = () => {
               image={player.photo}
               name={player.name}
               type={player.position}
+              navigate={() =>
+                navigateToPlayer(
+                  teamid as unknown as number,
+                  teamname as string,
+                  player.id
+                )
+              }
             />
           ))}
         </PlayersListContainer>
