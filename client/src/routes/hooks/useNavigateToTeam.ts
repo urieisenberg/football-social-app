ebc2ae50d1790ed6617065124e41379620028ddf
urieisenberg <urieisenberg@gmail.com>
2022-12-24 17:37:47 +0200

    add path

--- client/src/routes/hooks/useNavigateToTeam.ts
+++ client/src/routes/hooks/useNavigateToTeam.ts
@@ -1,6 +1,13 @@
 import { useNavigate } from 'react-router-dom';
 
-export const useNavigateToTeam = (teamid: string, teamname: string) => {
+export const useNavigateToTeam = (
+  teamid: string,
+  teamname: string,
+  path?: string
+) => {
   const navigate = useNavigate();
+  if (path) {
+    return () => navigate(`/team/${teamid}/${path}`);
+  }
   return () => navigate(`/team/${teamid}`);
 };
