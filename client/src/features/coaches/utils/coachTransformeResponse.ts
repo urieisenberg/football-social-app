a316782cea7f30fc06a4d2466c43a244b495b8b4
urieisenberg <urieisenberg@gmail.com>
2022-12-24 20:36:51 +0200

    transform for get coach by id

--- client/src/features/coaches/utils/coachTransformeResponse.ts
+++ client/src/features/coaches/utils/coachTransformeResponse.ts
@@ -1,3 +1,6 @@
+export const transformCoacByIdResponse = (response: any) => {
+  return response.response[0];
+};
 
 export const transformCoachResponse = (response: any) => {
   return response.response;
