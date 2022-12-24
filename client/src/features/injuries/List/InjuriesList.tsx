387869661c850547db9de83fd62597870b2dca1a
urieisenberg <urieisenberg@gmail.com>
2022-12-24 21:51:47 +0200

    useNavigateToPlayer too card

--- client/src/features/injuries/List/InjuriesList.tsx
+++ client/src/features/injuries/List/InjuriesList.tsx
@@ -1,3 +1,5 @@
+import { useParams } from 'react-router-dom';
+import { useNavigateToPlayer } from '../../../app/hooks';
 import { Injury } from '../types';
 import { Card } from '../../../components/Card';
 import { TopButton } from '../../../components/Button';
@@ -11,6 +13,8 @@ interface InjuriesListProps {
 }
 
 export const InjuriesList = ({ data, title }: InjuriesListProps) => {
+  const { teamid, teamname } = useParams();
+  const { navigateToPlayer } = useNavigateToPlayer();
   let content;
   if (data.length === 0)
     content = <InjuriesContainer>No Injuries Found</InjuriesContainer>;
@@ -24,7 +28,16 @@ export const InjuriesList = ({ data, title }: InjuriesListProps) => {
               injuries={injury}
               image={injury.player.photo}
               name={injury.player.name}
-              type={injuriesReasonsHandler(injury.player.reason as unknown as string)}
+              type={injuriesReasonsHandler(
+                injury.player.reason as unknown as string
+              )}
+              navigate={() =>
+                navigateToPlayer(
+                  teamid as unknown as number,
+                  teamname as string,
+                  injury.player.id
+                )
+              }
             />
           ))}
         </InjuriesPlayers>
