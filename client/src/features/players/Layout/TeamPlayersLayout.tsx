ad78781bca2f99ead1624075c03521cca1a3b4f6
urieisenberg <urieisenberg@gmail.com>
2022-12-24 21:51:56 +0200

    player item added

--- client/src/features/players/Layout/TeamPlayersLayout.tsx
+++ client/src/features/players/Layout/TeamPlayersLayout.tsx
@@ -1,17 +1,15 @@
-import {
-  useRoutes,
-  Outlet,
-  Navigate,
-} from 'react-router-dom';
+import { useRoutes, Outlet, Navigate } from 'react-router-dom';
 import { Nav } from '../../../components/Nav';
 import { SquadPlayersList } from '../list';
 import { InjuriesTeamList } from '../../injuries/list';
+import { PlayersItem } from '../item';
 
 export const TeamPlayersLayout = () => {
   let elements = useRoutes([
     { path: '', element: <Navigate to="squad" /> },
     { path: 'squad', element: <SquadPlayersList /> },
     { path: 'injuries', element: <InjuriesTeamList /> },
+    { path: ':playerid', element: <PlayersItem /> },
   ]);
 
   return (
