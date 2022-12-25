c20b9751e2e0d45ff68ff59ceccf229de6319389
urieisenberg <urieisenberg@gmail.com>
2022-12-26 00:42:00 +0200

    lineups init

---
+++ client/src/features/fixtures/item/lineups/index.tsx
@@ -0,0 +1,7 @@
+import { useParams } from 'react-router-dom';
+import { useGetFixtureLineupsQuery } from '../../api';
+import { Transition } from '../../../../components/Transition';
+
+export const FixtureLineups = () => {
+  return <div>FixtureLineups</div>;
+};
