dfd200b281a02c06116da33214b8fd26a30bb17c
urieisenberg <urieisenberg@gmail.com>
2022-12-25 16:35:05 +0200

    h2h api calls

---
+++ client/src/features/fixtures/list/headToHead/index.tsx
@@ -0,0 +1,19 @@
+import { useParams } from 'react-router-dom';
+import { useGetHeadToHeadQuery } from '../../api';
+import { Loader } from '../../../../components/Loader';
+import { FixturesList } from '..';
+
+export const HeadToHeadFixturesList = () => {
+    const { teamids } = useParams();
+
+    console.log(teamids);
+    const { data, error, isLoading, isSuccess } = useGetHeadToHeadQuery(teamids as string);
+
+    let content;
+    if (isLoading) content = <Loader />;
+    else if (error || !data) content = <>No fixtures found</>;
+    else if (isSuccess && data) content = <FixturesList fixtures={data} />;
+
+    return <>{content}</>;
+};
+
