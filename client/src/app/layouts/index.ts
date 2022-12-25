2f2af139a09e5a2dd3f5ab4ac184e0e8758cbc8e
urieisenberg <urieisenberg@gmail.com>
2022-12-25 15:24:39 +0200

    exports

---
+++ client/src/app/layouts/index.ts
@@ -0,0 +1,5 @@
+export * from '../../features/fixtures/layout';
+export * from '../../features/players/layout';
+export * from '../../features/coaches/layout';
+export * from '../../features/transfers/layout';
+export * from '../../features/social/layout';
