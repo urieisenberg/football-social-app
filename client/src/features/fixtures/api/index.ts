ccd3884a381d6cfe30e667c0a496d76b08e681d6
urieisenberg <urieisenberg@gmail.com>
2022-12-26 00:41:26 +0200

    fixtures api calls

--- client/src/features/fixtures/api/index.ts
+++ client/src/features/fixtures/api/index.ts
@@ -1,5 +1,13 @@
 import { footballApi } from '../../../app/services/football-api';
-import { Fixture, CurrentFixtureRequest } from '../../../app/types';
+import {
+  Fixture,
+  CurrentFixtureRequest,
+  Round,
+  FixtureLineup,
+  FixtureTeamStatistics,
+  FixtureEvent,
+  FixturesPlayersStatistics,
+} from '../../../app/types';
 import {
   transformGetFixturesResponse,
   transformCurrentRoundResponse,
@@ -8,7 +16,7 @@ import { date } from '../utils/getCurrentDate';
 
 export const fixturesApi = footballApi.injectEndpoints({
   endpoints: (build) => ({
-    getCurrentRound: build.query<string[], string>({
+    getCurrentRound: build.query<Round, string>({
       query: (leagueid) =>
         `fixtures/rounds?league=${leagueid}&season=2022&current=true`,
       transformResponse: transformCurrentRoundResponse,
@@ -16,6 +24,7 @@ export const fixturesApi = footballApi.injectEndpoints({
     getCurrentFixtures: build.query<Fixture[], CurrentFixtureRequest>({
       query: ({ leagueid, round }) =>
         `fixtures?league=${leagueid}&season=2022&round=${round}`,
+      transformResponse: transformGetFixturesResponse,
     }),
     getTeamNextFixtures: build.query<Fixture[], string>({
       query: (teamid) => `fixtures?team=${teamid}&next=50`,
@@ -30,6 +39,24 @@ export const fixturesApi = footballApi.injectEndpoints({
       query: (teamids) => `fixtures/headtohead?h2h=${teamids}`,
       transformResponse: transformGetFixturesResponse,
     }),
+    getFixtureLineups: build.query<FixtureLineup, string>({
+      query: (fixtureid) => `fixtures/lineups?fixture=${fixtureid}`,
+      transformResponse: transformGetFixturesResponse,
+    }),
+    getFixtureEvents: build.query<FixtureEvent[], string>({
+      query: (fixtureid) => `fixtures/events?fixture=${fixtureid}`,
+      transformResponse: transformGetFixturesResponse,
+    }),
+    getFixtureStatistics: build.query<FixtureTeamStatistics, string>({
+      query: (fixtureid) => `fixtures/statistics?fixture=${fixtureid}`,
+      transformResponse: transformGetFixturesResponse,
+    }),
+    getFixturePlayersStatistics: build.query<FixturesPlayersStatistics, string>(
+      {
+        query: (fixtureid) => `fixtures/players?fixture=${fixtureid}`,
+        transformResponse: transformGetFixturesResponse,
+      }
+    ),
   }),
   overrideExisting: false,
 });
@@ -40,4 +67,8 @@ export const {
   useGetTeamNextFixturesQuery,
   useGetTeamPreviousFixturesQuery,
   useGetHeadToHeadQuery,
+  useGetFixtureLineupsQuery,
+  useGetFixtureEventsQuery,
+  useGetFixtureStatisticsQuery,
+  useGetFixturePlayersStatisticsQuery,
 } = fixturesApi;
