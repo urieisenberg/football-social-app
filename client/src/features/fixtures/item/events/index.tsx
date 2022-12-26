152b138428d7ddc45c63613271a6e11c1ad4b632
urieisenberg <urieisenberg@gmail.com>
2022-12-26 18:16:35 +0200

    init

---
+++ client/src/features/fixtures/item/events/index.tsx
@@ -0,0 +1,40 @@
+import { useParams } from 'react-router-dom';
+import { useGetFixtureEventsQuery } from '../../api';
+import { useNavigateToPlayer } from '../../../../routes/hooks';
+import { filterEventsByTeam } from '../../utils/filterEventsByTeam';
+import { Transition } from '../../../../components/Transition';
+import { GiSoccerBall } from 'react-icons/gi';
+import {
+  EventsContainer,
+  EventsTitle,
+  EventsItem,
+  EventsLine,
+  EventsLink,
+  EventsPlayer,
+  EventsSubTitle,
+  EventsRating,
+} from '../../styles';
+
+interface FixtureEventsProps {
+  homeTeam: number;
+  awayTeam: number;
+}
+
+export const FixturesItemEvents = ({
+  homeTeam,
+  awayTeam,
+}: FixtureEventsProps) => {
+  const { fixture } = useParams();
+  const { data, isLoading } = useGetFixtureEventsQuery(fixture as string);
+  const { navigateToPlayer } = useNavigateToPlayer();
+
+  //   const { goals, cards, substitutions } = data
+
+  const { homeEvents, awayEvents } = filterEventsByTeam(
+    data,
+    homeTeam,
+    awayTeam
+  );
+
+  return <></>;
+};
