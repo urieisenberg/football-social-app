f5c802e24b446acf9721d8a6aff38f1b51a5c540
urieisenberg <urieisenberg@gmail.com>
2022-12-26 18:16:41 +0200

    init

--- client/src/features/fixtures/item/index.tsx
+++ client/src/features/fixtures/item/index.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
-
+import { useParams } from 'react-router-dom';
+import { FixturesItemEvents } from './events';
 export const FixturesItem = () => {
+  const { fixture } = useParams();
+
   return (
-    <div>FixtureItem</div>
-  )
-}
+    <div>
+      <FixturesItemEvents homeTeam={123123} awayTeam={121} />
+    </div>
+  );
+};
