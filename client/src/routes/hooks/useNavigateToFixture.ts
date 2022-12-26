5d0ef321f91fc8c0775c46d13ea6ecd0a8dbac10
urieisenberg <urieisenberg@gmail.com>
2022-12-26 15:58:54 +0200

    custom hook to nav to player page

--- client/src/routes/hooks/useNavigateToFixture.ts
+++ client/src/routes/hooks/useNavigateToFixture.ts
@@ -1,10 +1,15 @@
 import { useNavigate } from 'react-router-dom';
 
-export const useNavigateToFixture = (
-  teamid: string,
-  teamname: string,
-  fixtureid: string
-) => {
+export const useNavigateToFixture = () => {
   const navigate = useNavigate();
-  return () => navigate(`/team/${teamid}/fixture/${fixtureid}`);
+
+  const navigateToFixture = (
+    teamid: string,
+    teamname: string,
+    fixture: number
+  ) => {
+    navigate(`/team/${teamid}/${teamname}/fixtures/${fixture}`);
+  };
+
+  return { navigateToFixture };
 };
