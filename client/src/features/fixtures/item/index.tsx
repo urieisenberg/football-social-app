46f7c60a8024de793a4b553095da357e0d76d6df
urieisenberg <urieisenberg@gmail.com>
2023-01-06 20:48:24 +0200

    clean

--- client/src/features/fixtures/item/index.tsx
+++ client/src/features/fixtures/item/index.tsx
@@ -1,15 +1,7 @@
 import { useParams } from 'react-router-dom';
+import { useGetFixtureQuery } from '../api';
 import { FixtureEvents } from './events';
-import {
-  useNavigateToVenue,
-  useNavigateToTeam,
-  useNavigateToHeadToHead,
-} from '../../../routes/hooks';
-import { useGetFixtureStatisticsQuery, useGetFixtureQuery } from '../api';
-import {
-  FixturesContainer,
-  FixturesWrapper,
-} from '../styles';
+import { FixturesContainer, FixturesWrapper, FixturesHR } from '../styles';
 import { Transition } from '../../../components/Transition';
 import { Loader } from '../../../components/Loader';
 import { FixtureVenue } from './venue';
@@ -17,6 +9,7 @@ import { FixtureLeague } from './league';
 import { FixtureDate } from './date';
 import { FixtureLinks } from './links';
 import { FixtureScore } from './score';
+import { FixturePlayersStats } from './statistics/player';
 
 export const FixturesItem = () => {
   const { fixture } = useParams();
@@ -48,113 +41,16 @@ export const FixturesItem = () => {
               homeTeam={fixtureData.homeEvents}
               awayTeam={fixtureData.awayEvents}
             />
+            <FixturesHR />
+            {/* <FixturePlayersStats
+              playersDataAvailable={fixtureData.playersDataAvailable}
+              homeTeam={fixtureData.statistics[0]}
+              awayTeam={fixtureData.statistics[0]}
+            /> */}
           </FixturesContainer>
         </Transition>
       </FixturesWrapper>
     );
-  // else
-  //   content = (
-  //     <FixturesWrapper>
-  //       <Transition key={fixture + 'page'}>
-  //         <FixturesContainer>
-  //           <FixturesLeague>
-  //             {fixtureData.league.name + ' ' + fixtureData.league.round}
-  //           </FixturesLeague>
-  //           {/* <FixturesDate>
-  //             {new Date(fixtureData.fixture.fixture.date).toLocaleString()}
-  //           </FixturesDate> */}
-  //           <FixturesLeague>
-  //             Referee: {referee ? referee : 'TBD'}
-  //           </FixturesLeague>
-  //           <FixturesContent>
-  //             <FixturesLink>
-  //               <FixturesVenue
-  //                 onClick={() =>
-  //                   navigateToVenue(
-  //                     fixtureData.league.id,
-  //                     fixtureData.fixture.venue.id
-  //                   )
-  //                 }
-  //               >
-  //                 {venue ? venue : 'TBD'}
-  //               </FixturesVenue>
-  //             </FixturesLink>
-  //             <FixturesTeam>
-  //               <FixturesLink
-  //                 onClick={() =>
-  //                   navigateToTeam(
-  //                     fixtureData.fixture.teams?.home.id,
-  //                     fixtureData.fixture.teams?.home.name
-  //                   )
-  //                 }
-  //               >
-  //                 <FixturesLogo
-  //                   src={fixtureData.fixture.teams?.home.logo}
-  //                   alt={fixtureData.fixture.teams?.home.name}
-  //                 />
-  //                 <FixturesBR />
-  //                 {fixtureData.fixture.teams?.home.name}
-  //               </FixturesLink>
-  //             </FixturesTeam>
-  //             <FixturesScore>
-  //               {fixtureData.fixture.status.short === 'NS' ||
-  //               fixtureData.fixture.status.short === 'TBD' ? (
-  //                 <FixturesCountdown date={fixtureData.fixture.date} />
-  //               ) : (
-  //                 <FixturesGoals>
-  //                   {fixtureData.fixture.goals?.home +
-  //                     ' - ' +
-  //                     fixtureData.fixture.goals?.away}
-  //                 </FixturesGoals>
-  //               )}
-  //             </FixturesScore>
-  //             <FixturesTeam>
-  //               <FixturesLink
-  //                 onClick={() =>
-  //                   navigateToTeam(
-  //                     fixtureData.fixture.teams?.away.id,
-  //                     fixtureData.fixture.teams?.away.name
-  //                   )
-  //                 }
-  //               >
-  //                 <FixturesLogo
-  //                   src={fixtureData.fixture.teams?.away.logo}
-  //                   alt={fixtureData.fixture.teams?.away.name}
-  //                 />
-  //                 <FixturesBR />
-
-  //                 {fixtureData.fixture.teams?.away.name}
-  //               </FixturesLink>
-  //             </FixturesTeam>
-  //           </FixturesContent>
-  //           {/* <FixturesLink
-  //             onClick={() =>
-  //               navigateToHeadToHead(
-  //                 fixtureData.fixture.teams.home.id as unknown as string,
-  //                 fixtureData.fixture.teams.away.id as unknown as string,
-  //                 fixtureData.fixture.teams.home.id,
-  //                 fixtureData.fixture.teams.away.id,
-  //                 fixtureData.fixture.teams.home.name,
-  //                 fixtureData.fixture.teams.away.name
-  //               )
-  //             }
-  //           >
-  //             <FixturesH2H>
-  //               Click to see more games between{' '}
-  //               {fixtureData.fixture.teams.home.name} and{' '}
-  //               {fixtureData.fixture.teams.away.name}
-  //             </FixturesH2H>
-  //           </FixturesLink> */}
-  //           <FixturesHR />
-  //           <FixturesItemEvents
-  //             homeTeam={fixtureData.homeTeam.events}
-  //             awayTeam={fixtureData.awayTeam.events}
-  //             playersDataAvailable={fixtureData.playersDataAvailable}
-  //           />
-  //         </FixturesContainer>
-  //       </Transition>
-  //     </FixturesWrapper>
-  //   );
 
   return <>{content}</>;
 };
