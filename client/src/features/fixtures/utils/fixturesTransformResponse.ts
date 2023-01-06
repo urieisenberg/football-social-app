764bbb5c4dc2eb5cd18838bf00aab8b8151b477f
urieisenberg <urieisenberg@gmail.com>
2023-01-06 14:42:08 +0200

    new transform response for fixture to match props

--- client/src/features/fixtures/utils/fixturesTransformResponse.ts
+++ client/src/features/fixtures/utils/fixturesTransformResponse.ts
@@ -6,6 +6,17 @@ export const transformGetFixturesResponse = (response: any): any => {
 
 export const transformFixtureResponse = (response: any): any => {
   const data = response.response[0];
+  const fixture = {
+    fixture: data.fixture,
+    league: data.league,
+    teams: data.teams,
+    goals: data.goals,
+    score: data.score,
+  };
+  
+
+  const playersDataAvailable = data.fixture.date.includes('2022');
+
   const homeEvents = data.events.filter(
     (event: any) => event.team.id === data.teams.home.id
   );
@@ -26,46 +37,79 @@ export const transformFixtureResponse = (response: any): any => {
     (event: any) => event.type === 'Subst'
   );
 
-  const homeLineups = data.lineups.filter(
-    (lineup: any) => lineup.team.id === data.teams.home.id
-  );
-
-  const awayLineups = data.lineups.filter(
-    (lineup: any) => lineup.team.id === data.teams.away.id
-  );
-
-  const homeStatistics = data.statistics.filter(
-    (statistic: any) => statistic.team.id === data.teams.home.id
-  );
-
-  const awayStatistics = data.statistics.filter(
-    (statistic: any) => statistic.team.id === data.teams.away.id
-  );
-
-  const playersDataAvailable = data.fixture.date.includes('2022');
-
-  const homeTeam = {
-    ...data.teams.home,
-    events: goalsHome.concat(cardsHome, substitutionsHome),
-    lineup: homeLineups,
-    statistics: homeStatistics,
-  };
-
-  const awayTeam = {
-    ...data.teams.away,
-    events: goalsAway.concat(cardsAway, substitutionsAway),
-    lineup: awayLineups,
-    statistics: awayStatistics,
-  };
-
   return {
-    fixture: data.fixture,
-    league: data.league,
-    homeTeam,
-    awayTeam,
+    fixture,
     playersDataAvailable,
+    awayEvents: goalsAway.concat(cardsAway, substitutionsAway),
+    homeEvents: goalsHome.concat(cardsHome, substitutionsHome),
+    statistics: data.statistics,
+    lineup: data.lineups,
+    players: data.players,
   };
 };
+
+// export const transformFixtureResponse = (response: any): any => {
+//   const data = response.response[0];
+//   const homeEvents = data.events.filter(
+//     (event: any) => event.team.id === data.teams.home.id
+//   );
+
+//   const goalsHome = homeEvents.filter((event: any) => event.type === 'Goal');
+//   const cardsHome = homeEvents.filter((event: any) => event.type === 'Card');
+//   const substitutionsHome = homeEvents.filter(
+//     (event: any) => event.type === 'Subst'
+//   );
+
+//   const awayEvents = data.events.filter(
+//     (event: any) => event.team.id === data.teams.away.id
+//   );
+
+//   const goalsAway = awayEvents.filter((event: any) => event.type === 'Goal');
+//   const cardsAway = awayEvents.filter((event: any) => event.type === 'Card');
+//   const substitutionsAway = awayEvents.filter(
+//     (event: any) => event.type === 'Subst'
+//   );
+
+//   const homeLineups = data.lineups.filter(
+//     (lineup: any) => lineup.team.id === data.teams.home.id
+//   );
+
+//   const awayLineups = data.lineups.filter(
+//     (lineup: any) => lineup.team.id === data.teams.away.id
+//   );
+
+//   const homeStatistics = data.statistics.filter(
+//     (statistic: any) => statistic.team.id === data.teams.home.id
+//   );
+
+//   const awayStatistics = data.statistics.filter(
+//     (statistic: any) => statistic.team.id === data.teams.away.id
+//   );
+
+//   const playersDataAvailable = data.fixture.date.includes('2022');
+
+//   const homeTeam = {
+//     ...data.teams.home,
+//     events: goalsHome.concat(cardsHome, substitutionsHome),
+//     lineup: homeLineups,
+//     statistics: homeStatistics,
+//   };
+
+//   const awayTeam = {
+//     ...data.teams.away,
+//     events: goalsAway.concat(cardsAway, substitutionsAway),
+//     lineup: awayLineups,
+//     statistics: awayStatistics,
+//   };
+
+//   return {
+//     fixture: data.fixture,
+//     league: data.league,
+//     homeTeam,
+//     awayTeam,
+//     playersDataAvailable,
+//   };
+// };
 
 export const transformCurrentRoundResponse = (response: any): any => {
   const previousRound = getPreviousRound(response.response[0]);
