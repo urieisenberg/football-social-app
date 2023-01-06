7e8a45188d805f1eb0643e91daf365da5ec950ad
urieisenberg <urieisenberg@gmail.com>
2023-01-06 21:43:14 +0200

    players stats

--- client/src/features/fixtures/item/statistics/player/index.tsx
+++ client/src/features/fixtures/item/statistics/player/index.tsx
@@ -1,17 +1,17 @@
 import { useNavigateToPlayer } from '../../../../../routes/hooks';
 import { FixturesPlayersStatistics } from '../../../types';
+import { handleEventIcon } from '../../../utils/eventIcon';
 import { Transition } from '../../../../../components/Transition';
-import { GiSoccerBall } from 'react-icons/gi';
 import {
   EventsContainer,
   EventsItem,
-  EventsLink,
   EventsPlayer,
+  EventsRating,
 } from '../../../styles';
 
 interface FixturePlayersStatsProps {
   homeTeam: FixturesPlayersStatistics | undefined;
-  awayTeam: FixturesPlayersStatistics| undefined;
+  awayTeam: FixturesPlayersStatistics | undefined;
   playersDataAvailable: boolean | undefined;
 }
 
@@ -20,11 +20,70 @@ export const FixturePlayersStats = ({
   awayTeam,
   playersDataAvailable,
 }: FixturePlayersStatsProps) => {
-    const { navigateToPlayer } = useNavigateToPlayer();
+  const { navigateToPlayer } = useNavigateToPlayer();
 
-    console.log(homeTeam, awayTeam);
+  const handleIcons = (type: string, number: number) => {
+    for (let i = 0; i < number; i++) {
+      return handleEventIcon(type);
+    }
+  };
 
+  let content;
+  if (!homeTeam || !awayTeam) content = null;
+  else
+    content = (
+      <EventsContainer>
+        <EventsItem>
+          {homeTeam.players.map((p) => (
+            <EventsPlayer
+              key={p.player.id}
+              onClick={() =>
+                playersDataAvailable &&
+                navigateToPlayer(
+                  homeTeam.team.id,
+                  homeTeam.team.name,
+                  p.player.id
+                )
+              }
+            >
+              <EventsRating>{p.statistics[0].games.rating}</EventsRating>
+              {p.player.name}
+              <></>
+              {handleIcons('Red Card', p.statistics[0].cards.red)}
+              {handleIcons('Yellow Card', p.statistics[0].cards.yellow)}
+              {handleIcons('Goal', p.statistics[0].goals.total)}
+              {handleIcons('Foul', p.statistics[0].fouls.committed)}
+              {handleIcons('Saved', p.statistics[0].goals.saves)}
+            </EventsPlayer>
+          ))}
+        </EventsItem>
+        <EventsPlayer></EventsPlayer>
+        <EventsItem>
+          {awayTeam.players.map((p) => (
+            <EventsPlayer
+              key={p.player.id}
+              onClick={() =>
+                playersDataAvailable &&
+                navigateToPlayer(
+                  awayTeam.team.id,
+                  awayTeam.team.name,
+                  p.player.id
+                )
+              }
+            >
+              <EventsRating>{p.statistics[0].games.rating}</EventsRating>
+              {p.player.name}
+              <></>
+              {handleIcons('Red Card', p.statistics[0].cards.red)}
+              {handleIcons('Yellow Card', p.statistics[0].cards.yellow)}
+              {handleIcons('Goal', p.statistics[0].goals.total)}
+              {handleIcons('Foul', p.statistics[0].fouls.committed)}
+              {handleIcons('Saved', p.statistics[0].goals.saves)}
+            </EventsPlayer>
+          ))}
+        </EventsItem>
+      </EventsContainer>
+    );
 
-
-  return <div>FixturePlayersStats</div>;
+  return <Transition key="players stats">{content}</Transition>;
 };
