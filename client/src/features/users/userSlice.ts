a29e3dea606c5fa381c47dbeeca6009b512dc4f4
urieisenberg <urieisenberg@gmail.com>
2023-01-09 00:22:26 +0200

    test

--- client/src/features/users/userSlice.ts
+++ client/src/features/users/userSlice.ts
@@ -21,20 +21,11 @@ const userSlice = createSlice({
     deleteUser: (state) => {
       state.user = null;
     },
-    followUser: (state, action: PayloadAction<User>) => {
-      state.following.push(action.payload);
-      localStorageUser.following.push(action.payload);
-      localStorage.setItem('user', JSON.stringify(localStorageUser));
+    followUser: (state, action: PayloadAction<User[]>) => {
+      state.following = action.payload;
     },
-    unfollowUser: (state, action: PayloadAction<User>) => {
-      // const index = state.following.findIndex(
-      //   (user) => user._id === action.payload._id
-      // );
-      // state.following.splice(index, 1);
-      localStorageUser.following.splice(
-        localStorageUser.following.indexOf(action.payload),
-        1
-      );
+    unfollowUser: (state, action: PayloadAction<User[]>) => {
+      state.following = action.payload;
     },
     setFollowing: (state, action: PayloadAction<User[]>) => {
       state.following = action.payload;
