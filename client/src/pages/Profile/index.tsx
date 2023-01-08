106930f6a5d9cc6ba5d4595bd28a96ddd1ab1c95
urieisenberg <urieisenberg@gmail.com>
2023-01-08 19:07:36 +0200

    followers and following list

--- client/src/pages/Profile/index.tsx
+++ client/src/pages/Profile/index.tsx
@@ -1,13 +1,15 @@
 import { useRoutes, Outlet, Navigate } from 'react-router-dom';
 import { Nav } from '../../components/Nav';
 import { UserBar } from '../../features/users/bar';
+import { FollowersList } from '../../features/users/list/followers';
+import { FollowingList } from '../../features/users/list/following';
 
 export const Profile = () => {
   let elements = useRoutes([
     { path: '', element: <Navigate to="posts" /> },
     { path: 'posts', element: <></> },
-    { path: 'following', element: <></> },
-    { path: 'followers', element: <></> },
+    { path: 'following', element: <FollowingList /> },
+    { path: 'followers', element: <FollowersList /> },
     { path: 'likes', element: <></> },
     { path: 'fixtures', element: <></> },
   ]);
