9a007c5457cbb626f2522738774b69d5de1a1318
urieisenberg <urieisenberg@gmail.com>
2023-01-08 20:08:20 +0200

    item styled

---
+++ client/src/features/users/styles/Item.ts
@@ -0,0 +1,19 @@
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
+
+export const Item = styled(motion.a).attrs({
+  whileHover: {
+    scale: 1.1,
+    transition: {
+      duration: 0.3,
+    },
+  },
+  whileTap: {
+    scale: 0.9,
+  },
+})`
+  &:hover {
+    cursor: pointer;
+    color: ${({ theme }) => theme.primary};
+  }
+`;
