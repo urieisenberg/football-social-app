16544c26eb29228ae44c5150e1b94d78ef46839a
urieisenberg <urieisenberg@gmail.com>
2023-01-08 19:08:18 +0200

    render following list comp

---
+++ client/src/features/users/list/following/index.tsx
@@ -0,0 +1,41 @@
+import { useNavigate } from 'react-router-dom';
+import { useAppSelector } from '../../../../app/hooks';
+import { useGetFollowingQuery } from '../../api';
+import { ListWrapper, ListContainer, ListTitle, ListItem } from '../../styles';
+import { Loader } from '../../../../components/Loader';
+import { Card } from '../../../../components/Card';
+
+export const FollowingList = () => {
+  const navigate = useNavigate();
+  const { user } = useAppSelector((state) => state.user);
+
+  const { data, isLoading } = useGetFollowingQuery(user?._id as string);
+
+  let content;
+  if (isLoading) content = <Loader />;
+  else if (data?.length === 0)
+    content = <ListTitle> {user?.username} is not following anyone</ListTitle>;
+  else if (data && user)
+    content = (
+      <ListWrapper>
+        <ListContainer>
+          <ListTitle>
+            {user.following.length}{' '}
+            {user.following.length === 1 ? 'following' : 'following'}
+          </ListTitle>
+          <ListItem>
+            {data.map((following) => (
+              <Card
+                key={following._id}
+                image={following.profilePicture}
+                name={following.username}
+                navigate={() => navigate(`/users/${following.username}`)}
+              />
+            ))}
+          </ListItem>
+        </ListContainer>
+      </ListWrapper>
+    );
+
+  return <>{content}</>;
+};
