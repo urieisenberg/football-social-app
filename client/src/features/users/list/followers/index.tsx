a380f0ee3f8207822e7961329018cefc9ed5fd7a
urieisenberg <urieisenberg@gmail.com>
2023-01-08 19:08:07 +0200

    render followers list

--- client/src/features/users/list/followers/index.tsx
+++ client/src/features/users/list/followers/index.tsx
@@ -1,26 +1,47 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useAppSelector } from '../../../../app/hooks';
-import { useGetFollowersQuery, useGetUserQuery } from '../../api';
+import { useGetFollowersQuery } from '../../api';
 import {
   ListWrapper,
   ListContainer,
   ListTitle,
   ListItem,
-  ListSubtitle,
 } from '../../styles';
 import { Loader } from '../../../../components/Loader';
+import { Card } from '../../../../components/Card';
 
 export const FollowersList = () => {
-  const { username } = useParams();
-  const { user } = useAppSelector((state) => state.auth);
+  const navigate = useNavigate();
 
-  const { data: userData, isLoading: userIsLoading } = useGetUserQuery(
-    username as string
-  );
+  const { user } = useAppSelector((state) => state.user);
 
-  const { data, isLoading } = useGetFollowersQuery(userData?._id as string);
+  const { data, isLoading } = useGetFollowersQuery(user?._id as string);
 
   let content;
+  if (isLoading) content = <Loader />;
+  else if (data?.length === 0)
+    content = <ListTitle> {user?.username} has no followers</ListTitle>;
+  else if (data && user)
+    content = (
+      <ListWrapper>
+        <ListContainer>
+          <ListTitle>
+            {user.followers.length}{' '}
+            {user.followers.length === 1 ? 'follower' : 'followers'}
+          </ListTitle>
+          <ListItem>
+            {data.map((follower) => (
+              <Card
+                key={follower._id}
+                image={follower.profilePicture}
+                name={follower.username}
+                navigate={() => navigate(`/users/${follower.username}`)}
+              />
+            ))}
+          </ListItem>
+        </ListContainer>
+      </ListWrapper>
+    );
 
-  return <div>FollowersList</div>;
+  return <>{content}</>;
 };
