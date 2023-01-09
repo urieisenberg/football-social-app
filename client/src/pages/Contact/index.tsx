49d23dec61c07b47f35cd1a1efffcc71aeb70365
urieisenberg <urieisenberg@gmail.com>
2023-01-09 16:21:11 +0200

    settings added to nav

--- client/src/pages/Contact/index.tsx
+++ client/src/pages/Contact/index.tsx
@@ -1,4 +1,5 @@
-import { useRoutes, Outlet } from 'react-router-dom';
+import { useAppSelector } from '../../app/hooks';
+import { useRoutes, Outlet, Navigate } from 'react-router-dom';
 import { Nav } from '../../components/Nav';
 import { BackButton } from '../../components/Button';
 import {
@@ -6,21 +7,26 @@ import {
   TicketsList,
   TicketsMain,
   Ticket,
-  EditTicket
+  EditTicket,
 } from '../../features/tickets';
 
 export const Contact = () => {
+  const { user } = useAppSelector((state) => state.auth);
   let elements = useRoutes([
     { index: true, path: '', element: <TicketsMain /> },
     { path: 'tickets', element: <TicketsList /> },
     { path: 'create/ticket', element: <CreateTicket /> },
     { path: 'tickets/:ticketId', element: <Ticket /> },
-    { path: 'tickets/:ticketId/edit', element: <EditTicket />}
+    { path: 'tickets/:ticketId/edit', element: <EditTicket /> },
+    {
+      path: 'settings',
+      element: <Navigate to={`/account/${user?._id}/${user?.username}`} />,
+    },
   ]);
 
   return (
     <>
-      <Nav links={['tickets', 'create ticket']} />
+      <Nav links={['tickets', 'create ticket', 'settings']} />
       <BackButton size={30} />
       <Outlet />
       {elements}
