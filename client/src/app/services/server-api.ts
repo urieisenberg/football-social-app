5d37a7ecb67a7b01213a15e1a09124a20df0e487
urieisenberg <urieisenberg@gmail.com>
2023-01-09 17:05:17 +0200

    base url updated

--- client/src/app/services/server-api.ts
+++ client/src/app/services/server-api.ts
@@ -4,7 +4,7 @@ import { RootState } from '../store';
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3000/api',
+    baseUrl: '/api',
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).auth.token;
       if (token) {
