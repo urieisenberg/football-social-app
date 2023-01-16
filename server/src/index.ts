10c3ca53c74be701913bd0fd51d917fa0faf739a
urieisenberg <urieisenberg@gmail.com>
2023-01-16 18:38:45 +0200

    clean

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,20 +19,6 @@ app.use(cors());
 app.use(morgan(':method :url :status :response-time ms'.bgWhite));
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  if (req.method === 'OPTIONS') {
-    res.send();
-  } else {
-    next();
-  }
-});
-
 app.use('/api/auth', authRoutes);
 app.use('/api/tickets', ticketRoutes);
 app.use('/api/posts', postRoutes);
