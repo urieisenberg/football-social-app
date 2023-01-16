d07832bda9c3dfe1bd5c14a6df51d717e924009d
urieisenberg <urieisenberg@gmail.com>
2023-01-16 20:45:33 +0200

    baseUrl

--- client/src/app/services/server-api.ts
+++ client/src/app/services/server-api.ts
@@ -1,10 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { RootState } from '../store';
 
+const URL = process.env.REACT_APP_FOOTBALL_API_SERVER;
+
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://football-social-api.vercel.app/api',
+    baseUrl: `${URL}api`,
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).auth.token;
       if (token) {
