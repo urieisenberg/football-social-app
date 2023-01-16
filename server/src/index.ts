7ec9dc036d74a5ebcf3ba2e8ff1b1c5e36ac78ba
urieisenberg <urieisenberg@gmail.com>
2023-01-16 18:36:31 +0200

    cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,10 +13,33 @@ const port = process.env.PORT || 4747;
 dotevn.config();
 connectDB();
 
+// const corsOptions = {
+//   origin: '*',
+//   optionsSuccessStatus: 200,
+//   credentials: true,
+//   accessControlAllowOrigin: '*',
+//   accessControlAllowHeaders: '*',
+//   accessControlAllowMethods: '*',
+// };
+
 app.options('*', cors());
 app.use(cors());
 app.use(morgan(':method :url :status :response-time ms'.bgWhite));
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.send();
+  } else {
+    next();
+  }
+});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/tickets', ticketRoutes);
