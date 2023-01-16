ca43e48729a91610acf753e0becc6765f0705611
urieisenberg <urieisenberg@gmail.com>
2023-01-16 17:06:54 +0200

    env name fixed

--- server/src/config/token.ts
+++ server/src/config/token.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 export const generateToken = (id: string): string => {
   return jwt.sign({ id }, process.env.JWT_SECRET as string, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
+    expiresIn: process.env.JWT_EXP,
   });
 };
 
